81d71a442411bd08bdb4061450a5c3aa1cb7e9c4
Paul Okello <paulokello273@gmail.com>
2020-09-13 16:37:49 +0300

    Render footer and logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,16 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Logo from './components/Logo/Logo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <main className="app-content">
+        <Footer />
+      </main>
+
     </div>
   );
 }
